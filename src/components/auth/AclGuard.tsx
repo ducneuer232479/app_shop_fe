f4469ac4581c5f9501ce88c4a19491bdfda801c5
ducneuer232479 <tuanduc.vu249@gmail.com>
2025-02-20 16:38:52 +0700

    111. Phân quyền cho dự án trong Nextjs 14 (Phân quyền phần 1)

--- src/components/auth/AclGuard.tsx
+++ src/components/auth/AclGuard.tsx
@@ -3,7 +3,13 @@
 import { ReactNode } from 'react'
 
 // ** Types
-import type { ACLObj } from 'src/configs/acl'
+import { buildAbilityFor, type ACLObj, type AppAbility } from 'src/configs/acl'
+
+import BlankLayout from 'src/views/layouts/BlankLayout'
+import NotAuthorized from 'src/pages/401'
+import { useAuth } from 'src/hooks/useAuth'
+import { useRouter } from 'next/router'
+import { AbilityContext } from '../acl/Can'
 
 interface AclGuardProps {
   children: ReactNode
@@ -15,8 +21,37 @@ interface AclGuardProps {
 const AclGuard = (props: AclGuardProps) => {
   // ** Props
   const { aclAbilities, children, guestGuard = false, authGuard = true } = props
-  
-  return <>{children}</>
+
+  const auth = useAuth()
+  const permissionUser = auth.user?.role.permissions ?? []
+  const router = useRouter()
+
+  let ability: AppAbility
+
+  if (auth.user && !ability) {
+    ability = buildAbilityFor(permissionUser, aclAbilities.subject)
+  }
+
+  // if guest guard or no guard is true (khong can dang nhap) or any error page
+  if (guestGuard || router.route === '/500' || router.route === '/404' || !authGuard) {
+    if (auth.user && ability) {
+      return <AbilityContext.Provider value={ability}>{children}</AbilityContext.Provider>
+    } else {
+      return children
+    }
+  }
+
+  // check the access of current user
+  if (ability && auth.user && ability.can(aclAbilities.action, aclAbilities.subject)) {
+    return <AbilityContext.Provider value={ability}>{children}</AbilityContext.Provider>
+  }
+
+  // return <>{children}</>
+  return (
+    <BlankLayout>
+      <NotAuthorized />
+    </BlankLayout>
+  )
 }
 
 export default AclGuard
